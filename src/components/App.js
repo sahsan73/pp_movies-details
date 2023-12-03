1762a0e686fd65949867ae78831d6e12f79befb4
Saquib Ahsan <saquibahsan73@gmail.com>
2023-12-03 19:29:14 +0530

    build layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
+import { useState } from "react";
 import "../index.css";
+import Main from "./Main";
 import NavBar from "./NavBar";
 
-/*
 const tempMovieData = [
   {
     imdbID: "tt1375666",
@@ -26,30 +27,13 @@ const tempMovieData = [
   },
 ];
 
-const tempWatchedData = [
-  {
-    imdbID: "tt1375666",
-    Title: "Inception",
-    Year: "2010",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: "tt0088763",
-    Title: "Back to the Future",
-    Year: "1985",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
-*/
-
 export default function App() {
-  return <NavBar />;
+  const [movies, setMovies] = useState(tempMovieData);
+
+  return (
+    <>
+      <NavBar movies={movies} />
+      <Main movies={movies} />
+    </>
+  );
 }
