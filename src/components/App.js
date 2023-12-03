4fc3d2367b81d14d3dd3b0d42ca636a5f32e0de3
Saquib Ahsan <saquibahsan73@gmail.com>
2023-12-03 22:26:46 +0530

    create reusable star rating component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import { useState } from "react";
-import "../index.css";
+import { useEffect, useState } from "react";
 import NavBar from "./NavBar";
 import Main from "./Main";
-import ListBox from "./ListBox";
+import Box from "./Box";
 import MoviesList from "./MoviesList";
-import WatchedBox from "./WatchedBox";
+import WatchedSummary from "./WatchedSummary";
+import WatchedMoviesList from "./WatchedMoviesList";
 
 const tempMovieData = [
   {
@@ -29,18 +29,44 @@ const tempMovieData = [
       "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
   },
 ];
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
       <NavBar movies={movies} />
       <Main>
-        <ListBox>
+        <Box>
           <MoviesList movies={movies} />
-        </ListBox>
-        <WatchedBox />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </Box>
       </Main>
     </>
   );
